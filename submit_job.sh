242a4e5857326b3c6086365741c350e7e67cfa6e
Alexandros Giavaras <a.giavaras@gmail.com>
2020-03-06 14:34:50 +0000

    Add submit_jobb script

---
+++ submit_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#PBS -N hmmTraining
+#PBS -l walltime=00:02:00
+#PBS -l pvmem=1gb
+#PBS -m bea
+#PBS -M ag568@leicester.ac.uk
+
+# Make python  available
+module load python3
+
+# Execute the MPI job code
+python3 hidden_markov_modeling/analysis.py
