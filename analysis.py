44821903cd81b1bf269e0e7d2e3f9f3f85ac2101
Alexandros Giavaras <a.giavaras@gmail.com>
2020-03-06 15:00:32 +0000

    Add print message

--- analysis.py
+++ analysis.py
@@ -4,14 +4,16 @@ from _helpers import read_configuration_file
 
 def main():
 
-    description = "Check the README file for information on how to use the script"
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('--config', type=str, default='config.json',
-                        help='You must specify a json formatted configuration file')
-    args = parser.parse_args()
+    #description = "Check the README file for information on how to use the script"
+    #parser = argparse.ArgumentParser(description=description)
+    #parser.add_argument('--config', type=str, default='config.json',
+    #                    help='You must specify a json formatted configuration file')
+    #args = parser.parse_args()
 
-    config_file = args.config
-    configuration = read_configuration_file(config_file)
+    #config_file = args.config
+    #configuration = read_configuration_file(config_file)
+    print("Started job")
+    print("Finished job")
 
 if __name__ == '__main__':
     main()
