e360f86de802e7ef35888560cad8fde410b44f25
Alexandros Giavaras <a.giavaras@gmail.com>
2020-03-09 12:41:24 +0000

    Refactro main function

--- analysis.py
+++ analysis.py
@@ -1,7 +1,9 @@
 import argparse
 import pysam
-from _helpers import read_configuration_file
-
+from helpers import read_configuration_file
+from bam_helpers import extract_windows
+from preprocess_utils import  preprocess
+from hmm import HMM
 
 def main():
 
@@ -17,7 +19,24 @@ def main():
     print("Configuration: ", configuration)
 
     try:
-        samfile = pysam.AlignmentFile('m605_verysensitive_trim_sorted.bam',"rb")
+
+        # read the refernce  file
+        ref_file = pysam.FastaFile(configuration["reference_file"],"rb")
+
+        # read the test file
+        test_file = pysam.AlignmentFile(configuration["reference_file"],"rb")
+
+        # extract the windows
+        windows = extract_windows(chromosome=configuration["chromosome"], ref_file=ref_file,
+                                  start_test=test_file, **{"start_test": configuration["start_test"],
+                                                           "end_test": configuration["end_test"]})
+        # apply preprocessing for the windows
+        windows = preprocess(windows=windows)
+
+        # specify the HMM model
+        hmm = HMM(start_transitions=configuration["HMM"]["initial_transitions_p"])
+        hmm.fit(dataset=windows, solver=configuration["HMM"]["train_solver"])
+
     except Exception as e:
         print( str(e))
 
