9b069c2bcf8899438ae53257a4a10a17c5c194a8
Alexandros Giavaras <a.giavaras@gmail.com>
2020-03-11 11:23:41 +0000

    fix bugs with forward/backward implementation

--- tests/test_forward_backward.py
+++ tests/test_forward_backward.py
@@ -4,13 +4,13 @@ Unit tests for forward-backward algorithms
 import unittest
 import numpy as np
 from forward_backward import forward
+from forward_backward import backward
 
 class TestForwardBackward(unittest.TestCase):
 
     def test_forward(self):
         """
-        Test forward algorith.
-        :return:
+        Test forward algorithm.
         """
         observations = np.array([3, 1, 3])
 
@@ -26,7 +26,24 @@ class TestForwardBackward(unittest.TestCase):
 
         obs_indices = {1: 0, 2: 1, 3: 2}
 
-        alpha = forward(dataset=observations, A=A, B=B, initial_dist=initialprob)
+        alpha = forward(obs_indices=obs_indices, observations=observations,
+                        A=A, B=B, initial_dist=initialprob)
+
+    def test_backward(self):
+        """
+        Test backward algorithm.
+        """
+        observations = np.array([3, 1, 3])
+
+        # transition matrix
+        A = np.array([[0.7, 0.3],
+                      [0.4, 0.6]])
+        # emission matrix
+        B = np.array([[0.2, 0.4, 0.4],
+                      [0.5, 0.4, 0.1]])
+
+        obs_indices = {1: 0, 2: 1, 3: 2}
+        beta = backward(obs_indices=obs_indices, dataset=observations, A=A, B=B)
 
 
 if __name__ == '__main__':
