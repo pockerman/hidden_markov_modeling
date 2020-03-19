758bbcbc9e8e81e9d8fa86d3231ba86de67262b2
Alexandros Giavaras <a.giavaras@gmail.com>
2020-03-19 15:53:43 +0000

    Update main function

--- analysis.py
+++ analysis.py
@@ -1,10 +1,16 @@
 import argparse
 import pysam
+import logging
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from pomegranate import*
+
 from helpers import read_configuration_file
+from helpers import set_up_logger
 from bam_helpers import extract_windows
 from exceptions import Error
-from preprocess_utils import  preprocess
-from hmm import HMM
+
 
 def main():
 
@@ -18,12 +24,13 @@ def main():
     config_file = args.config
     configuration = read_configuration_file(config_file)
 
-    with pysam.AlignmentFile(configuration["reference_file"]["name"],"rb") as ref_file:
+    # configure the logger to use
+    set_up_logger(configuration=configuration)
+    logging.info("Checking logger...")
 
-        print("\t Reference file")
-        print("\t", ref_file.filename)
-        print("\t", ref_file.description)
-        print("\n")
+    with pysam.FastaFile(configuration["reference_file"]["name"]) as ref_file:
+
+        print("\t Reference file: ", ref_file.filename)
 
         with pysam.AlignmentFile(configuration["test_file"]["filename"],"rb") as test_file:
 
@@ -51,30 +58,69 @@ def main():
                         "windowsize":int(windowsize)}
 
                 # extract the windows
-                windows = extract_windows(chromosome=chromosome, ref_file=ref_file,
-                                      test_file=test_file, **args)
+                windows = extract_windows(chromosome=chromosome,
+                                          ref_file=ref_file,
+                                          test_file=test_file, **args)
 
-                if(len(windows) == 0):
+                if len(windows) == 0:
                     raise Error("No windows have been created")
                 else:
                     print("\t\tNumber of windows: ", len(windows))
 
-                print("Finished analysis")
-
-                """
-                # apply preprocessing for the windows
-                windows = preprocess(windows=windows)
-    
-                # specify the HMM model
-                hmm = HMM(start_transitions=configuration["HMM"]["initial_transitions_p"])
-                hmm.fit(dataset=windows, solver=configuration["HMM"]["train_solver"])
-                """
             except KeyError as e:
-                print("Key: " + str(e) + " does not exist")
+                logging.error("Key: {0} does not exit".format(str(e)))
             except Error as e:
-                print("Error occured: " + str(e))
+                logging.error(str(e))
             except Exception as e:
-                print("Unknown exception occured: " + str(e))
+                logging.error("Unknown exception occured: " + str(e))
+
+    print("Extracted dataset....")
+    print("Finished analysis")
+
+    """
+    sns.set(color_codes=True)
+
+    # accumulate all RD observations
+    rd_observations = []
+
+    # now that we have the windows lets do some EDA
+    print("Number of windows: ", len(windows))
+
+    # get basic statistics from the windos
+    for idx, window in enumerate(windows):
+        print("Window id ", idx)
+        stats = window.get_rd_stats(statistics="all")
+        print("\t mean: ",   stats["mean"])
+        print("\t var: ",    stats["var"])
+        print("\t median: ", stats["median"])
+
+        rd_data = window.get_rd_observations()
+        rd_observations.extend(rd_data)
+        sns.distplot(rd_data)
+        plt.show()
+
+    mean = np.mean(rd_observations)
+    var = np.var(rd_observations)
+    median = np.median(rd_observations)
+
+    print("mean: ", mean)
+    print("var: ", var)
+    print("median: ", median)
+
+    sns.distplot(rd_observations)
+    plt.show()
+
+    # uniform distribution generated
+    # from the available data
+    uniform_dist = UniformDistribution.from_samples(rd_observations)
+    uniform_dist.plot(1000, edgecolor='c', color='c', bins=20)
+    plt.show()
+    #print("Uniform dist mean: ", uniform_dist.mu)
+    #print("Uniform dist var: ", uniform_dist.cov)
+    """
+
+
+
 
 
 
