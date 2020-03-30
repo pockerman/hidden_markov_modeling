da21194434e52fcc8cae0c2575c13d0140f9632c
Alexandros Giavaras <a.giavaras@gmail.com>
2020-03-30 16:58:53 +0100

    Add ZScore clusterer

--- preprocess_utils.py
+++ preprocess_utils.py
@@ -3,8 +3,10 @@ Preprocessing utilities
 """
 
 from pomegranate import*
+import scipy.stats as st
 
 from exceptions import Error
+from helpers import windows_rd_statistics
 
 VALID_DISTS = ['gaussian', 'uniform', 'poisson']
 
@@ -32,28 +34,6 @@ def fit_distribution(data, dist_name="gaussian", **kwargs):
         dist = PoissonDistribution.from_samples(data)
         return dist
 
-
-def preprocess(windows, **config):
-    """
-    Apply preprocessing to the given windows
-    by using the specified configuration
-    :param windows:
-    :param config:
-    :return:
-    """
-
-    if "remove_outliers" in config.keys():
-        remove_outliers(windows=windows, criteria=config["remove_outliers_criteria"])
-
-    if "normalize_windows_data" in config.keys():
-        remove_outliers(windows=windows, criteria=config["normalize_windows_criteria"])
-
-    if "normality_check" in config.keys():
-        for name in config["normality_check"]:
-            normality_check(windows=windows, test_name=name)
-    return windows
-
-
 def cluster(data, nclusters, method, **kwargs):
 
   """
@@ -70,32 +50,123 @@ def cluster(data, nclusters, method, **kwargs):
     clusterer = KMeans(n_clusters=nclusters)
     clusterer.fit(data)
     return clusterer
+  elif "zscore":
+    return z_score_window_clusterer(windows=data,
+                                    n_consecutive_windows=kwargs["n_consecutive_windows"],
+                                    selector=kwargs["selector"])
+
 
   raise Error("Invalid clustering method: " + method )
 
 
+def calculate_windows_zscore(windows):
+  """
+  Returns an array of Z-scores for every window
+  in the given windows list
 
-def remove_outliers(windows, criteria):
-    """
-    Remove the outlier windows from the list of the
-    windows by using the given criteria
-    :return:
-    """
+  Parameters
+  ----------
+  windows : list of Window instances
+    DESCRIPTION.
+
+  Returns
+  -------
+  list of floats representing the z-score for each window
+
+  """
+
+  mu = windows_rd_statistics(windows, statistic="mean")
+  var = windows_rd_statistics(windows, statistic="var")
+
+  z_scores = []
+
+  for window in windows:
+    zscore = (window.get_rd_stats(statistics="mean") - mu)/var
+    z_scores.append(zscore)
+
+  return z_scores
+
+def windows_tails_p(zscores, l):
+  """
+  Calculate windows upper and lower tail probabilities arranged
+  into window intervals of length l
+
+
+  Parameters
+  ----------
+  zscores :  list
+    DESCRIPTION. The score corresponding to each window
+  l : int
+    Length of window interval
+
+  Returns
+  -------
+  upper_ps, lower_ps : list of upper probabilities,
+                        list of lower probabilities
+
+  """
+
+  upper_ps = []
+  lower_ps = []
+  local_probs_up = []
+  local_probs_low = []
+
+  window_counter = 0
+  for i, zscore in enumerate(zscores):
+
+    if len(local_probs_up) < l:
+      prob = st.norm.cdf(zscore)
+      local_probs_up.append({"idx":i,
+                             "prob":prob})
+
+      local_probs_low.append({"idx":i,
+                              "prob":1.0-prob})
+
+    else:
+      upper_ps.append(local_probs_up)
+      lower_ps.append(local_probs_low)
+      local_probs_up = []
+      local_probs_low =[]
+
+      prob = st.norm.cdf(zscore)
+      prob = st.norm.cdf(zscore)
+      local_probs_up.append({"idx":i,
+                             "prob":prob})
+
+      local_probs_low.append({"idx":i,
+                              "prob":1.0-prob})
+
+  return upper_ps, lower_ps
+
+
+
+def z_score_window_clusterer(windows, n_consecutive_windows, selector):
+  """
+  classify each window according to the given states
+  by computing a Z-score and a cut off quantity
+  """
+
+  zscores = calculate_windows_zscore(windows=windows)
+  upper_ps, lower_ps = windows_tails_p(scores=zscores,
+                                       l=n_consecutive_windows)
+
+  # we do the clustering
+
+  for window in windows:
+    selector(window, upper_ps, lower_ps)
+
+  return windows
+
+
+class ZScoreWindowCluster(object):
+
+
+  def __init__(self):
     pass
 
-
-def normalize_windows_data(windows, criteria):
-    """
-    normalize the data in the given windows list
-    """
-    pass
+  def __call__(self, window, upper_ps, lower_ps):
 
 
-def normality_check(windows, test_name):
-    """
-    Perform a normality test, specified by the test_name variable, for the given windows
-    :param windows: The list of windows to use
-    :param test_name: The name of the statistical test to use
-    :return:
-    """
-    pass
+
+
+
