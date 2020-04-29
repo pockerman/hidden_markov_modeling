d2ad5529dbe40c44df162e96af26008519afcc8b
Alexandros Giavaras <a.giavaras@gmail.com>
2020-04-29 09:52:47 +0100

    Fix bug

--- analysis.py
+++ analysis.py
@@ -284,7 +284,7 @@ def hmm_train(clusters, windows, configuration):
   observations = []
 
   for window in windows:
-    observations.append(window.get_gc_percent(windowtype="n_wga_w"))
+    observations.append([window.get_gc_percent(windowtype="n_wga_w")])
 
   print("{0} Done...".format(INFO))
   print("{0} Fit HMM...".format(INFO))
