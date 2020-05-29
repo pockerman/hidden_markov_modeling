d64844b5a8838d055773d607bde3e41081a55ddc
Alexandros Giavaras <a.giavaras@gmail.com>
2020-05-29 10:38:14 +0100

    NOOP

--- bam_helpers.py
+++ bam_helpers.py
@@ -41,6 +41,7 @@ def extract_windows(chromosome, ref_filename, bam_filename, **args):
 
             wcounter = 0
             while start_idx < end_idx:
+
               sam_output = window_sam_file(chromosome=chromosome,
                                        sam_file=sam_file,
                                        fastafile=fastafile,
@@ -51,12 +52,8 @@ def extract_windows(chromosome, ref_filename, bam_filename, **args):
                                     samdata=sam_output))
 
               start_idx += windowcapacity
-
-              #print("{0} Created window: {1}".format(INFO, wcounter))
-
               wcounter += 1
-              #if wcounter == 1:
-              #  break
+
     return windows
 
 
